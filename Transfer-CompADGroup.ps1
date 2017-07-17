9becd2d53e49a1a8f403f509c9acc57d02254c1d
jamiewhitehead <jamie.whitehead2@gmail.com>
2017-07-17 22:10:56 +1000

    Add files via upload

---
+++ Transfer-CompADGroup.ps1
@@ -0,0 +1,60 @@
+﻿PARAM
+    (
+    [String]$SrcComputerName,
+    [String]$TrgtComputerName
+    )
+  
+# ####################################################################################
+# ####     MAIN CODE                                                            ######
+# ####################################################################################
+
+# ------------------------------
+# Prepare the ADSI Search object
+# ------------------------------
+
+$dom = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$root = $dom.GetDirectoryEntry()
+$search = [System.DirectoryServices.DirectorySearcher]$root
+
+# ---------------------------------------------------------------------------------
+# Get the list of groups (distinguishedName) that the Source machine is a member of
+# ---------------------------------------------------------------------------------
+
+$search.Filter = "(&(objectCategory=Computer)(SamAccountname=$($SrcComputerName)`$))"
+$SrcADMachineGroupList = $search.FindOne().Properties.memberof
+
+# ---------------------------------
+# Get the target computer ADSI Path
+# ---------------------------------
+
+$search.Filter = "(cn=$TrgtComputerName)"
+$TrgtADMachine = $search.FindOne().GetDirectoryEntry().Path
+
+# --------------------------------------------------------
+# Add the target computer to each of the discovered Groups
+# --------------------------------------------------------
+
+ForEach($ADGroup in $SrcADMachineGroupList)
+{
+    $search.Filter = "(distinguishedName=$ADGroup)"
+    $ADGroupObj = [ADSI]$search.FindOne().Path
+    Try
+    {
+        $ADGroupObj.psbase.Invoke("Add",$TrgtADMachine)
+    }
+    Catch
+    {
+        If($($_.Exception) -match "The object already exists")
+        {
+            #No Action to take. Computer is already a member of the group. 
+        }       
+        Else
+        {
+            $_.Exception > "C:\Get-AdGroup4Comp.log"
+        }
+    }
+}
+
+# ####################################################################################
+# ####     END OF MAIN CODE                                                     ######
+# #################################################################################### 
